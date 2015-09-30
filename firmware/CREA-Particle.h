3116710a4f41aa39afbc8a0b72f0942e40e9bfe1
Dahngeek <me@dahngeek.com>
2015-09-29 19:35:54 -0600

    headers remove

--- firmware/CREA-Particle.h
+++ firmware/CREA-Particle.h
@@ -65,7 +65,6 @@ String WebSocketClientStringTable = {
 			"Origin: SparkWebSocketClient\x0d\x0a"
 			"Sec-WebSocket-Key1: 18x 6]8vM;54 *(5:  {   U1]8  z [  8\x0d\x0a"
       "Sec-WebSocket-Key2: 1_ tx7X d  <  nw  334J702) 7]o}` 0\x0d\x0a"
-			"Sec-WebSocket-Version: 13\x0d\x0a"
       "HTTP/1.1 101\x0d\x0a"
 			"\x0d\x0a"};
   const char* _hostname;
